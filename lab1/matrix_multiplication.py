ee649d1a043297314d06d2d186e2b3301134bf30
mjste <mj.stefanik@gmail.com>
2022-11-06 22:35:10 +0100

    updated binet

--- lab1/matrix_multiplication.py
+++ lab1/matrix_multiplication.py
@@ -2,15 +2,21 @@ from math import log, ceil
 import random
 import time
 
+
 def binet(A, B, d=None):
     if d == None:
         K, L, M = len(A), len(B), len(B[0])
-    else: K, L, M = d
+    else:
+        K, L, M = d
 
-    if K == 0: return ([], 0)
-    if L == 0: return ([[0 for j in range(M)] for i in range(K)], 0)
-    if M == 0: return ([[]], 0)
-    if K == 1 and L == 1 and M == 1: return ([[A[0][0] * B[0][0]]], 1)
+    if K == 0:
+        return ([], 0)
+    if L == 0:
+        return ([[0 for j in range(M)] for i in range(K)], 0)
+    if M == 0:
+        return ([[]], 0)
+    if K == 1 and L == 1 and M == 1:
+        return ([[A[0][0] * B[0][0]]], 1)
 
     A11 = [[A[i][j] for j in range(0, L // 2)] for i in range(0, K // 2)]
     A12 = [[A[i][j] for j in range(L // 2, L)] for i in range(0, K // 2)]
@@ -24,17 +30,17 @@ def binet(A, B, d=None):
 
     C = [[0 for j in range(M)] for i in range(K)]
 
-    A11B11 = binet(A11, B11, (K // 2, L // 2, M // 2))
-    A12B21 = binet(A12, B21, (K // 2, L - L // 2, M // 2))
+    A11B11, c1 = binet(A11, B11, (K // 2, L // 2, M // 2))
+    A12B21, c2 = binet(A12, B21, (K // 2, L - L // 2, M // 2))
 
-    A11B12 = binet(A11, B12, (K // 2, L // 2, M - M // 2))
-    A12B22 = binet(A12, B22, (K // 2, L - L // 2, M - M // 2))
+    A11B12, c3 = binet(A11, B12, (K // 2, L // 2, M - M // 2))
+    A12B22, c4 = binet(A12, B22, (K // 2, L - L // 2, M - M // 2))
 
-    A21B11 = binet(A21, B11, (K - K // 2, L // 2, M // 2))
-    A22B21 = binet(A22, B21, (K - K // 2, L - L // 2, M // 2))
+    A21B11, c5 = binet(A21, B11, (K - K // 2, L // 2, M // 2))
+    A22B21, c6 = binet(A22, B21, (K - K // 2, L - L // 2, M // 2))
 
-    A21B12 = binet(A21, B12, (K - K // 2, L // 2, M - M // 2))
-    A22B22 = binet(A22, B22, (K - K // 2, L - L // 2, M - M // 2))
+    A21B12, c7 = binet(A21, B12, (K - K // 2, L // 2, M - M // 2))
+    A22B22, c8 = binet(A22, B22, (K - K // 2, L - L // 2, M - M // 2))
 
     for i in range(K // 2):
         for j in range(M // 2):
@@ -52,7 +58,8 @@ def binet(A, B, d=None):
         for j in range(M - M // 2):
             C[K // 2 + i][M // 2 + j] += A21B12[i][j] + A22B22[i][j]
 
-    return C
+    cost = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + K * M
+    return (C, cost)
 
 
 def add_m(A, B):
@@ -176,23 +183,24 @@ def test():
          [3, 3, 3, 3],
          [4, 4, 4, 4]]
 
-    a = binet(C, D)
+    a, ac = binet(C, D)
     b = strassen_r(C, D)
     c = strassen(C, D)
     assert a == b == c
 
-    a = binet(E, F)
+    a, ac = binet(E, F)
     b = strassen_r(E, F)
     c = strassen(E, F)
     assert a == b == c
 
-    a = binet(A, B)
+    a, ac = binet(A, B)
     c = strassen(A, B)
     assert a == c
 
+
 def measure():
-    file_strassen = open('strassen.txt', 'w')
-    file_binet = open('binet.txt', 'w')
+    # file_strassen = open('strassen.txt', 'w')
+    # file_binet = open('binet.txt', 'w')
 
     random.seed(42)
     for i in range(2, 10):
@@ -203,23 +211,20 @@ def measure():
         start = time.time()
         C = strassen(A, B)
         stop = time.time()
-        file_strassen.write(f'{i} {stop-start}\n')
-        print(f'i: {i}, k: {k}, time: {stop-start}')
+        # file_strassen.write(f'i: {i} {stop-start}\n')
+        print(f'strassen, i: {i}, k: {k}, time: {stop-start}, operations: {0}')
 
         start = time.time()
-        D = binet(A, B)
+        D, dc = binet(A, B)
         stop = time.time()
-        file_binet.write(f'{i} {stop-start}\n')
-        print(f'i: {i}, k: {k}, time: {stop-start}')
 
+        # file_binet.write(f'i: {i} {stop-start}\n')
+        print(f'binet, i: {i}, k: {k}, time: {stop-start}, operations: {dc}')
 
-    file_strassen.close()
-    file_binet.close()
-
-    
+    # file_strassen.close()
+    # file_binet.close()
 
 
 if __name__ == '__main__':
     test()
     measure()
-    
